295d3e57e2828eb748634edc444cdd01d166d163
Toby Batch <tobias@neontribe.co.uk>
2026-08-04 13:29:22 +0100

    Add nunjucks temlating engine
    We are steadly adding more rendering to this component. Nunjuks is used in other govuk sites, this commit adds it here.

--- src/@types/declarations.d.ts
+++ src/@types/declarations.d.ts
@@ -1,3 +1,13 @@
+declare module "*.njk" {
+  const content: string;
+  export default content;
+}
+
+declare module "*?raw" {
+  const content: string;
+  export default content;
+}
+
 declare module "*.scss" {
   const content: { readonly [className: string]: string };
   export default content;
