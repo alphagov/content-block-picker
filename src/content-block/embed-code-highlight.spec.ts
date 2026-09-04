f86d5ff61993ce4cb779562dbc82bd926eafd2dc
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-09-04 12:09:45 +0100

    Add functionality to find all embed codes
    Introduce a method to parse the contents of the textarea and return a
    match for any embed codes, in order to be able to find and manipulate
    these in the upcoming commits.

--- src/content-block/embed-code-highlight.spec.ts
+++ src/content-block/embed-code-highlight.spec.ts
@@ -33,6 +33,27 @@ Something else <mark class="content-block-highlight__mark">{{embed:content_block
     });
   });
 
+  describe("embedCodeMatches", () => {
+    it("should return one RegExpExecArray for each embed code in the document", () => {
+      const matches = embedCodeHighlight.embedCodeMatches(textWithEmbedCodes);
+
+      expect(matches.length).toBe(2);
+
+      const [match1, match2] = matches;
+
+      // Checking that the matches are RegExpExecArray objects with the expected properties
+      // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec#return_value
+      // (Basically a standard array with some extra properties like index and input)
+      expect(match1[0]).toBe(mockEmbedCode1);
+      expect(match1.index).toBeDefined();
+      expect(match1.input).toBeDefined();
+
+      expect(match2[0]).toBe(mockEmbedCode2);
+      expect(match2.index).toBeDefined();
+      expect(match2.input).toBeDefined();
+    });
+  });
+
   describe("update", () => {
     it("should sanitise the input text before processing", async () => {
       const mockHighlightAllEmbedCodes = vi.spyOn(
