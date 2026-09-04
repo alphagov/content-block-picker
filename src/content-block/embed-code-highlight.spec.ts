c289cdc58665c7c2d68014c41b32e3cab4771a08
Graham MacGregor <graham.macgregor@digital.cabinet-office.gov.uk>
2026-09-04 12:24:03 +0100

    Highlight invalid codes
    Introduce functionality to check the validity of each embed code and
    highlight each instance accordingly.
    
    Calls the API with each embed code and inspects the result to see if
    the response is 'valid'. If the response is not valid then we assume
    the embed code itself was also not valid and mark it as invalid in the
    overlay.
    
    Valid codes are highlighted as normal.

--- src/content-block/embed-code-highlight.spec.ts
+++ src/content-block/embed-code-highlight.spec.ts
@@ -1,9 +1,12 @@
 import { describe, it, expect, beforeEach, vi, afterEach } from "vitest";
 import { EmbedCodeHighlight } from "./embed-code-highlight";
+import { APIClient } from "./api-client";
+import { EmbedCodePreview } from "../@types";
 
 describe("EmbedCodeHighlight", () => {
+  const mockApiClient = new APIClient("https://example.com");
   let overlay = document.createElement("div");
-  let embedCodeHighlight = new EmbedCodeHighlight(overlay);
+  let embedCodeHighlight = new EmbedCodeHighlight(overlay, mockApiClient);
   const mockEmbedCode1 = "{{embed:content_block_contact:example}}";
   const mockEmbedCode2 = "{{embed:content_block_contact:bad}}";
 
@@ -12,9 +15,20 @@ This is some text.
 ${mockEmbedCode1}
 Something else ${mockEmbedCode2} here too.`;
 
+  const mockEmbedCodePreview: EmbedCodePreview = {
+    html: "<div>Preview content</div>",
+    valid: true,
+    error: null,
+  };
+  const mockEmbedCodeInvalidPreview: EmbedCodePreview = {
+    html: null,
+    valid: false,
+    error: new Error("Invalid embed code"),
+  };
+
   beforeEach(() => {
     overlay = document.createElement("div");
-    embedCodeHighlight = new EmbedCodeHighlight(overlay);
+    embedCodeHighlight = new EmbedCodeHighlight(overlay, mockApiClient);
   });
 
   afterEach(() => {
@@ -51,11 +65,40 @@ Something else <mark class="content-block-highlight__mark">{{embed:content_block
     });
   });
 
+  describe("highlightEmbedCodeValidity", () => {
+    it("should highlight valid embed codes in the document", () => {
+      const text = `${mockEmbedCode1}`;
+      const matches = embedCodeHighlight.embedCodeMatches(text);
+      const previews = [mockEmbedCodePreview];
+
+      embedCodeHighlight.highlightEmbedCodeValidity(text, matches, previews);
+
+      expect(embedCodeHighlight["overlay"].innerHTML).toContain(
+        '<mark class="content-block-highlight__mark">',
+      );
+    });
+
+    it("should highlight invalid embed codes in the document", () => {
+      const text = `${mockEmbedCode1}`;
+      const matches = embedCodeHighlight.embedCodeMatches(text);
+      const previews = [mockEmbedCodeInvalidPreview];
+
+      embedCodeHighlight.highlightEmbedCodeValidity(text, matches, previews);
+
+      expect(embedCodeHighlight["overlay"].innerHTML).toContain(
+        '<mark class="content-block-highlight__mark--invalid">',
+      );
+    });
+  });
+
   describe("update", () => {
     it("should sanitise the input text before processing", async () => {
       const mockHighlightAllEmbedCodes = vi.spyOn(
         embedCodeHighlight,
         "highlightAllEmbedCodes",
+      );
+      vi.spyOn(mockApiClient, "fetchPreview").mockResolvedValue(
+        mockEmbedCodePreview,
       );
 
       const unsanitizedText = `<div>${mockEmbedCode1}</div>`;
@@ -68,11 +111,38 @@ Something else <mark class="content-block-highlight__mark">{{embed:content_block
     });
 
     it("should highlight all embed codes immediately, without waiting for API responses", () => {
+      vi.spyOn(mockApiClient, "fetchPreview").mockResolvedValue(
+        mockEmbedCodePreview,
+      );
       embedCodeHighlight.update(`${mockEmbedCode1}`); // no await
 
       expect(embedCodeHighlight["overlay"].innerHTML).toBe(
         `<mark class="content-block-highlight__mark">{{embed:content_block_contact:example}}</mark>`,
       );
+    });
+
+    it("should return early when no embed codes are found", async () => {
+      const mockHighlightInvalidEmbedCodes = vi.spyOn(
+        embedCodeHighlight,
+        "highlightEmbedCodeValidity",
+      );
+      await embedCodeHighlight.update("This text has no embed codes.");
+
+      expect(mockHighlightInvalidEmbedCodes).not.toHaveBeenCalled();
+    });
+
+    it("should highlight invalid embed codes", async () => {
+      vi.spyOn(mockApiClient, "fetchPreview")
+        .mockResolvedValueOnce(mockEmbedCodePreview)
+        .mockResolvedValueOnce(mockEmbedCodeInvalidPreview);
+
+      await embedCodeHighlight.update(`
+Valid:   ${mockEmbedCode1}
+Invalid: {{embed:content_block_contact:bad}}`);
+
+      expect(overlay.innerHTML).toBe(`
+Valid:   <mark class="content-block-highlight__mark">{{embed:content_block_contact:example}}</mark>
+Invalid: <mark class="content-block-highlight__mark--invalid">{{embed:content_block_contact:bad}}</mark>`);
     });
   });
 });
--- src/content-block/embed-code-highlight.ts
+++ src/content-block/embed-code-highlight.ts
@@ -1,21 +1,57 @@
 import embedRegex from "./regex.ts";
+import { EmbedCodePreview } from "../@types";
+import { APIClient } from "./api-client.ts";
 import nunjucksEnv from "../nunjucks-env.ts";
 import markTemplate from "../templates/mark.njk?raw";
 
 export class EmbedCodeHighlight {
   constructor(
     private overlay: HTMLElement,
+    private apiClient: APIClient,
   ) {}
 
   async update(text: string) {
     const sanitisedText = this.sanitiseText(text);
     this.highlightAllEmbedCodes(sanitisedText);
+
+    const embedCodeMatches = this.embedCodeMatches(sanitisedText);
+    if (embedCodeMatches.length === 0) return;
+
+    const previews = await this.fetchEmbedCodeValidity(embedCodeMatches);
+    if (previews.length === 0) return;
+
+    this.highlightEmbedCodeValidity(sanitisedText, embedCodeMatches, previews);
   }
 
   highlightAllEmbedCodes(text: string) {
     this.overlay.innerHTML = text.replace(embedRegex, (embedCode) =>
       this.createMark(embedCode),
     );
+  }
+
+  highlightEmbedCodeValidity(
+    text: string,
+    embedCodeMatches: RegExpExecArray[],
+    previews: EmbedCodePreview[],
+  ) {
+    let result = "";
+    let lastIndex = 0;
+
+    embedCodeMatches.forEach((match, index) => {
+      const preview = previews[index];
+      const matchingText = match[0];
+      const matchStart = match.index;
+      const matchEnd = matchStart + matchingText.length;
+
+      result += text.slice(lastIndex, matchStart);
+      result += this.createMark(matchingText, preview.valid);
+
+      lastIndex = matchEnd;
+    });
+
+    result += text.slice(lastIndex);
+
+    this.overlay.innerHTML = result;
   }
 
   embedCodeMatches(text: string) {
@@ -32,5 +68,17 @@ export class EmbedCodeHighlight {
   private createMark(text: string, valid: boolean = true) {
     const modifier = valid ? "" : "--invalid";
     return nunjucksEnv.renderString(markTemplate, { modifier, text }).trim();
+  }
+
+  private async fetchEmbedCodeValidity(
+    embedCodeMatches: RegExpMatchArray[],
+  ): Promise<EmbedCodePreview[]> {
+    const previews = await
+      Promise.all(
+        embedCodeMatches.map((match) =>
+          this.apiClient.fetchPreview(match[0]),
+        ),
+      );
+    return previews;
   }
 }
